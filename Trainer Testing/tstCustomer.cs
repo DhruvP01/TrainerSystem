bd61cfdeaba8d0f8829a090b6c9bcc11bd7099a3
Dhruv Patel <P2588087@my365.dmu.ac.uk>
2022-02-07 10:35:08 +0000

    Added testing for customer class

---
+++ Trainer Testing/tstCustomer.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using TrainerClasses;
+
+namespace Trainer_Testing
+{
+    [TestClass]
+    public class tstCustomer
+    {
+        [TestMethod]
+        public void InstanceOk()
+        {
+            // create an instance of the class we want to create 
+            clsCustomer ACustomer = new clsCustomer();
+            // test to see that is exists
+            Assert.IsNotNull(ACustomer);
+        }
+    }
+
+    class clsCustomer
+    {
+    }
+}
+
+
