5bfc44c171a533ea3bbc542cb65d5230bbcfb3d2
Charlie Dodd <P2534487@my365.dmu.ac.uk>
2022-02-16 13:47:11 +0000

    Created staff test and class

---
+++ Trainer Testing/tstStaff.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Trainer_Testing
+{
+    [TestClass]
+    public class TstStaff
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            ClsStaff AnStaff = new ClsStaff();
+                Assert.IsNotNull(AnStaff);
+        }
+    }
+
+    class ClsStaff
+    {
+    }
+}
