29b1fdc5eaf19f6e68d10de6f6455ca8a06a3aee
Dhruv Patel <P2588087@my365.dmu.ac.uk>
2022-02-28 15:35:07 +0000

    Fixed errors on clsCustomerCollection

--- TrainerFrontOffice/Default.aspx.cs
+++ TrainerFrontOffice/Default.aspx.cs
@@ -7,8 +7,27 @@ using System.Web.UI.WebControls;
 
 public partial class _Default : System.Web.UI.Page
 {
+    // this function handles the load event for the page
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        // if this is the first time the page is displayed
+        if (IsPostBack == false)
+        {
+            // update the list box
+            DisplayCustomers();
+        }
+    }
+    void DisplayCustomers()
+    {
+        // create an instance of the customer collection 
+        TrainerClasses.clsCustomerCollection Customers = new TrainerClasses.clsCustomerCollection();
+        // set the data source to the list of customers in the collection
+        lstCustomers.DataSource = Customers.CustomerList;
+        // set the name of the primary key
+        lstCustomers.DataValueField = "CustomerNo";
+        // set the data field to display 
+        lstCustomers.DataTextField = "PostCode";
+        // bind the data to the list
+        lstCustomers.DataBind();
     }
 }
