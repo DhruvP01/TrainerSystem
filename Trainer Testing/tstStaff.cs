ef51531998b21011ca8d2fd27c9a56b888c23dd9
Charlie Dodd <P2534487@my365.dmu.ac.uk>
2022-03-14 13:02:27 +0000

    Revert "Added staff testing and began presentation layer for staff"
    This reverts commit af794d1f72fa76eb1525f4c59627a5699705b431.

--- Trainer Testing/tstStaff.cs
+++ Trainer Testing/tstStaff.cs
@@ -11,63 +11,8 @@ namespace Trainer_Testing
         public void TestMethod1()
         {
             ClsStaff AnStaff = new ClsStaff();
-            Assert.IsNotNull(AnStaff);
-        }
-        
-        [TestMethod]
-        public void TestMethod2()
-        {
-            ClsStaff AnStaff = new ClsStaff();
-            Boolean TestData = true;
-            AnStaff.Active = TestData;
-            Assert.AreEqual(AnStaff.Active, TestData);
-        }
-
-        [TestMethod]
-        public void TestMethod3()
-        {
-            ClsStaff AnStaff = new ClsStaff();
-            DateTime TestData = DateTime.Now.Date;
-            AnStaff.DateAdded = TestData;
-            Assert.AreEqual(AnStaff.DateAdded, TestData);
-        }
-
-        [TestMethod]
-        public void TestMethod4()
-        {
-            ClsStaff AnStaff = new ClsStaff();
-            Int32 TestData = 1;
-            AnStaff.StaffNo = TestData;
-            Assert.AreEqual(AnStaff.StaffNo, TestData);
-        }
-
-        [TestMethod]
-        public void TestMethod5()
-        {
-            ClsStaff AnStaff = new ClsStaff();
-            string TestData = "LE3 4GH";
-            AnStaff.PostCode = TestData;
-            Assert.AreEqual(AnStaff.PostCode, TestData);
-        }
-        
-        [TestMethod]
-        public void TestMethod6()
-        {
-            ClsStaff AnStaff = new ClsStaff();
-            string TestData = "123 Working Road";
-            AnStaff.Address = TestData;
-            Assert.AreEqual(AnStaff.Address, TestData);
-        }
-        
-        [TestMethod]
-        public void TestMethod7()
-        {
-            ClsStaff AnStaff = new ClsStaff();
-            string TestData = "Leicester";
-            AnStaff.Town = TestData;
-            Assert.AreEqual(AnStaff.Town, TestData);
+                Assert.IsNotNull(AnStaff);
         }
     }
-
 
 }
