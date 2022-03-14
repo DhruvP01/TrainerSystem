af794d1f72fa76eb1525f4c59627a5699705b431
Charlie Dodd <P2534487@my365.dmu.ac.uk>
2022-03-14 12:59:14 +0000

    Added staff testing and began presentation layer for staff

--- TrainerClasses/ClsStaff.cs
+++ TrainerClasses/ClsStaff.cs
@@ -1,6 +1,15 @@
-﻿namespace TrainerClasses
+﻿using System;
+
+namespace TrainerClasses
 {
     public class ClsStaff
     {
+        public bool Active { get; set; }
+        public DateTime DateAdded { get; set; }
+        public object StaffNo { get; set; }
+        public string PostCode { get; set; }
+        public string StaffAdress { get; set; }
+        public string Town { get; set; }
+        public string Address { get; set; }
     }
 }
