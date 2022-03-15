41ab8fcc3d42a1cc4b3b4a45403a1025eb7c0d8d
Charlie Dodd <P2534487@my365.dmu.ac.uk>
2022-03-15 13:10:21 +0000

    Creating the find method middle layer

--- Trainer Testing/tstStaff.cs
+++ Trainer Testing/tstStaff.cs
@@ -13,9 +13,9 @@ namespace Trainer_Testing
             ClsStaff AnStaff = new ClsStaff();
             Assert.IsNotNull(AnStaff);
         }
-        
+
         [TestMethod]
-        public void TestMethod2()
+        public void TestActive()
         {
             ClsStaff AnStaff = new ClsStaff();
             Boolean TestData = true;
@@ -24,7 +24,7 @@ namespace Trainer_Testing
         }
 
         [TestMethod]
-        public void TestMethod3()
+        public void TestDateAdded()
         {
             ClsStaff AnStaff = new ClsStaff();
             DateTime TestData = DateTime.Now.Date;
@@ -33,7 +33,7 @@ namespace Trainer_Testing
         }
 
         [TestMethod]
-        public void TestMethod4()
+        public void TestStaffNo()
         {
             ClsStaff AnStaff = new ClsStaff();
             Int32 TestData = 1;
@@ -42,32 +42,120 @@ namespace Trainer_Testing
         }
 
         [TestMethod]
-        public void TestMethod5()
+        public void TestPostCode()
         {
             ClsStaff AnStaff = new ClsStaff();
             string TestData = "LE3 4GH";
             AnStaff.PostCode = TestData;
             Assert.AreEqual(AnStaff.PostCode, TestData);
         }
-        
+
         [TestMethod]
-        public void TestMethod6()
+        public void TestAddress()
         {
             ClsStaff AnStaff = new ClsStaff();
             string TestData = "123 Working Road";
             AnStaff.Address = TestData;
             Assert.AreEqual(AnStaff.Address, TestData);
         }
-        
+
         [TestMethod]
-        public void TestMethod7()
+        public void TestTown()
         {
             ClsStaff AnStaff = new ClsStaff();
             string TestData = "Leicester";
             AnStaff.Town = TestData;
             Assert.AreEqual(AnStaff.Town, TestData);
         }
+
+        [TestMethod]
+        public void TestMethod8()
+        {
+            ClsStaff AnStaff = new ClsStaff();
+            Boolean found = false;
+            Int32 StaffNo = 21;
+            found = AnStaff.Find(StaffNo);
+            Assert.IsTrue(found);
+
+        }
+
+        [TestMethod]
+        public void TestMethod9()
+        {
+            ClsStaff AnStaff = new ClsStaff();
+            Boolean found = false;
+            Boolean OK = true;
+            Int32 StaffNo = 21;
+            found = AnStaff.Find(StaffNo);
+            if (AnStaff.StaffNo != 21)
+            {
+                OK = false;
+            }
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+        public void TestMethod10()
+        {
+            ClsStaff AnStaff = new ClsStaff();
+            Boolean found = false;
+            Boolean OK = true;
+            Int32 StaffNo = 21;
+            found = AnStaff.Find(StaffNo);
+            if (AnStaff.Address != "Test Street")
+            {
+                OK = false;
+            }
+            Assert.IsTrue(OK);
+
+        }
+
+        [TestMethod]
+        public void TestMethod11()
+        {
+            ClsStaff AnStaff = new ClsStaff();
+            Boolean found = false;
+            Boolean OK = true;
+            Int32 StaffNo = 21;
+            found = AnStaff.Find(StaffNo);
+            if (AnStaff.Town != "Test Town")
+            {
+                OK = false;
+            }
+            Assert.IsTrue(OK);
+
+        }
+
+        [TestMethod]
+        public void TestMethod12()
+        {
+            ClsStaff AnStaff = new ClsStaff();
+            Boolean found = false;
+            Boolean OK = true;
+            Int32 StaffNo = 21;
+            found = AnStaff.Find(StaffNo);
+            if (AnStaff.PostCode != "abc 123")
+            {
+                OK = false;
+            }
+            Assert.IsTrue(OK);
+
+        }
+
+        [TestMethod]
+        public void TestMethod13()
+        {
+            ClsStaff AnStaff = new ClsStaff();
+            Boolean found = false;
+            Boolean OK = true;
+            Int32 StaffNo = 21;
+            found = AnStaff.Find(StaffNo);
+            if (AnStaff.DateAdded != Convert.ToDateTime("24/09/2019"))
+            {
+                OK = false;
+            }
+            Assert.IsTrue(OK);
+
+        }
     }
-
-
 }
